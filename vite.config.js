b6de575f90ac3c1860ce1fc1061bae349fb8dec1
Salina Mendoza <89085544+salinamendoza@users.noreply.github.com>
2025-02-06 15:59:48 -0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,8 +3,13 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  root: 'src',
+  base: './',
   build: {
-    outDir: '../dist'
+    outDir: 'dist',
+    rollupOptions: {
+      input: {
+        main: './index.html'
+      }
+    }
   }
 })
